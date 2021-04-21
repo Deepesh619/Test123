4ffa7b127e85abc1ba19fcaf5e79aee43c2b7218
Deepesh619 <d.pralhad.dhande@accenture.com>
2021-04-21 18:12:59 +0530

    Activityjs changed c to C for configration

--- public/js/Activity.js
+++ public/js/Activity.js
@@ -4,10 +4,10 @@ var connection = new Postmonger.Session();
 connection.trigger('ready');
 
 connection.on('initActivity',function(data){
-    document.getElementById('configuration').value=JSON.stringify(data);
+    document.getElementById('Configuration').value=JSON.stringify(data);
 }); 
 
 connection.on('clickedNext',function(){
-    var configuration=JSON.parse(document.getElementById('configuration').value);
+    var configuration=JSON.parse(document.getElementById('Configuration').value);
     connection.trigger('updateActivity', configuration);
 })
