1f9ece78fa58592ecd1524e4d22f27a515cd3e29
Deepesh619 <d.pralhad.dhande@accenture.com>
2021-04-23 01:09:44 +0530

    c

--- public/js/Activity.js
+++ public/js/Activity.js
@@ -1,7 +1,7 @@
 
-/*var express = require('express')
+var express = require('express')
 var app = express()
-const axios = require('axios');*/
+const axios = require('axios');
 var authTokens = {};
 var payload = [
     {
