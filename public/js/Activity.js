6ba3fa67b90d683f714f5b33f1973642759e3c08
Deepesh619 <d.pralhad.dhande@accenture.com>
2021-04-23 18:16:28 +0530

    c

--- public/js/Activity.js
+++ public/js/Activity.js
@@ -1,6 +1,6 @@
 
-const express = require('express');
-//import express from './express';
+//const express = require('express');
+import express from './express';
  //= require('express')
 const app = express()
 //const axios = require('axios'); 
