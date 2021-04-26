b1d8df0095048f95939b3f63be27ebf8288caa9f
Deepesh <d.pralhad.dhande@accenture.com>
2021-04-26 18:04:00 +0530

    c

--- routes/activity.js
+++ routes/activity.js
@@ -90,7 +90,7 @@ var MCHost = 'mcllzpmqql69yd9kvcz1n-mj1fqy.rest.marketingcloudapis.com';
 var MCEndpoint = '/hub/v1/dataevents/key:D4627FEA-5CDE-4641-9AEA-A6EF9DA8881F/rowset';
 var rowData = [{
   "keys":{
-      "ContactKey": "0033B00000V9hUFAW"
+      "ContactKey": "12345678"
           },
   "values":{
       "Email":"akhil.passi122@accenture.com"
@@ -146,6 +146,7 @@ function  performPostRequest(endpoint,host,headers, method, data, success) {
 exports.execute = function (req, res) {
   
       console.log('This is start of execution- : '+req);
+      logData(req);
     performPostRequest(authEndpoint,authHost,authHeaders, method, authData, function(data) {
         accesstoken = data.access_token;
         console.log('Access token is: ', accesstoken);
