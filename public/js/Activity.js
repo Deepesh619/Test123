2075d090bb29d807897e516a518a54167541c6a9
Deepesh619 <d.pralhad.dhande@accenture.com>
2021-04-26 22:41:24 +0530

    c

--- public/js/Activity.js
+++ public/js/Activity.js
@@ -29,7 +29,7 @@ connection.on('initActivity',function(data){
     if (data) {
         payload = data;
     }
-    document.getElementById('DEName').value='Enter DE External Key';
+  //  document.getElementById('DEName').value='Enter DE External Key';
 }); 
 
 connection.on('clickedNext',function(){
