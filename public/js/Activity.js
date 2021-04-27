8ddcda5b8d6c27e1a9fcde16c2fb5f0f34ca0eb7
Deepesh619 <d.pralhad.dhande@accenture.com>
2021-04-27 23:14:01 +0530

    c

--- public/js/Activity.js
+++ public/js/Activity.js
@@ -1,30 +1,8 @@
 
-//const express = require(['express']);
-//import express from '../../node_modules/express';
- //= require('express')
-//const app = app();
-//const axios = require('axios');
-//import app from '../../app.js'; 
-//console.log(app);
-/*var querystring = require('querystring');
-var https = require('https');
-
-var payload = [
-    {
-        "keys":{
-            "ContactKey": "0033B00000V9hUFQAZ"
-                },
-        "values":{
-            "Email":"akhil.passi12@accenture.com"
-                }
-    }];*/
-    var authTokens = {};
     var payload = {};
     var connection = new Postmonger.Session();
 
 connection.trigger('ready');
-connection.trigger('requestTokens');
-connection.on('requestedTokens', onGetTokens);
 connection.on('initActivity',function(data){
     console.log(data);
     if (data) {
@@ -64,7 +42,6 @@ connection.on('clickedNext',function(){
     var destCloumnName2 = document.getElementById('destCloumnName2').value;
     console.log('DEName is : '+ DEName);
     payload['arguments'].execute.inArguments = [{
-        "tokens": authTokens,
         "srcCloumnValue1" : "{{Event."+ eventDefinitionKey +"."+srcCloumnName1+"}}",
         "srcCloumnValue2": "{{Event."+ eventDefinitionKey +"."+srcCloumnName2+"}}",
         "srcCloumnName1" : srcCloumnName1,
@@ -83,38 +60,3 @@ connection.on('clickedNext',function(){
     connection.trigger('updateActivity', payload);
 });
 
-
-function onGetTokens(tokens) {
-    console.log("Authtoken from Activity : "+tokens);
-    authTokens = tokens;
-}
-
-function insertDE() {
-    
-  /*  app.set('port', (process.env.PORT || 5000))
-    app.use(__dirname + '/public')
-
-    app.get('/connecttoMCData', function(request, responsefromWeb) {
-	
-        axios({;
-            method: 'post',
-            url: 'https://www.exacttargetapis.com/hub/v1/dataevents/key:CustomActivityTest/rowset',
-            data: payload,
-            headers:{
-               'Authorization': 'Bearer ' + authTokens,
-               'Content-Type': 'application/json',
-            }
-          })
-            .then(function(response) {
-                    var json = response;
-              console.log(json);
-              responsefromWeb.send(json);
-            }) 
-             .catch(function (error) {
-                console.log(error);
-            });
-    })
-
-    console.log(payload); */
-}
-//module.exports.insertDE=insertDE;
