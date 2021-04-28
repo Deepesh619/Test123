5579d09f12fc1bbbfdb91a6f40077b1fdef8c96f
Deepesh <d.pralhad.dhande@accenture.com>
2021-04-28 12:42:18 +0530

    c

--- public/js/Activity.js
+++ public/js/Activity.js
@@ -1,54 +1,6 @@
-const { save } = require("../../routes/activity");
 
     var payload = {};
     var connection = new Postmonger.Session();
-    //Deepesh
-    var lastStepEnabled=false;
-    var currentStep = steps[0].key;
-   
-    $(window).ready(onRender);
-
-    connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
-
-    connection.on('clickedNext', onClickedNext);
-    connection.on('clickedBack', onClickedBack);
-    connection.on('gotoStep', onGotoStep);
-
-    function onRender() {
-        // JB will respond the first time 'ready' is called with 'initActivity'
-        connection.trigger('ready');
-
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
-
-        // Disable the next button if a value isn't selected
-        $('#select1').change(function () {
-            var message = getMessage();
-            connection.trigger('updateButton', { button: 'next', enabled: Boolean(message) });
-
-            $('#message').html(message);
-        });
-
-        // Toggle step 4 active/inactive
-        // If inactive, wizard hides it and skips over it during navigation
-        $('#toggleLastStep').click(function () {
-            lastStepEnabled = !lastStepEnabled; // toggle status
-            steps[3].active = !steps[3].active; // toggle active
-
-            connection.trigger('updateSteps', steps);
-        })
-    }
-
-
-
-
-
-
-
-
-    // Deepesh end
 
 connection.trigger('ready');
 connection.on('initActivity',function(data){
@@ -56,15 +8,6 @@ connection.on('initActivity',function(data){
     if (data) {
         payload = data;
     }
-//Deepesh
-var steps = [
-    { "label": "Step 1", "key": "step1" },
-    { "label": "Step 2", "key": "step2" },
-    { "label": "Step 3", "key": "step3" },
-    { "label": "Step 4", "key": "step4", "active": false }]
-
-    // Deepesh end
-
     document.getElementById('DEName').value= payload['arguments'].execute.inArguments[0].DEName;
     document.getElementById('srcCloumnName1').value= payload['arguments'].execute.inArguments[0].srcCloumnName1;
     document.getElementById('pkDestCloumnName1').value= payload['arguments'].execute.inArguments[0].pkDestCloumnName1;
@@ -89,17 +32,6 @@ function(eventDefinitionModel) {
 
 
 connection.on('clickedNext',function(){
-    //Deepesh
-        if (
-            (currentStep.key === 'step3' && steps[3].active === false) ||
-                currentStep.key === 'step4'
-        ) {
-            save();
-        }
-
-    else //Deepesh end
-    {
-
     var DEName = document.getElementById('DEName').value;
     var srcCloumnName1 = document.getElementById('srcCloumnName1').value;
     var pkDestCloumnName1 = document.getElementById('pkDestCloumnName1').value;
@@ -122,69 +54,6 @@ connection.on('clickedNext',function(){
     payload['key'] = 'REST-1';
     payload['type'] = 'REST';
    // console.log('Total PAyload: - '+JSON.stringify(payload));
-   connection.trigger('nextStep');
-    //connection.trigger('updateActivity', payload);
-}
-
-//Deepesh till end
-function showStep(step, stepIndex) {
-    if (stepIndex && !step) {
-        step = steps[stepIndex - 1];
-    }
-
-    currentStep = step;
-
-    $('.step').hide();
-
-    switch (currentStep.key) {
-        case 'step1':
-            $('#step1').show();
-            connection.trigger('updateButton', {
-                button: 'next',
-                enabled: Boolean(getMessage())
-            });
-            connection.trigger('updateButton', {
-                button: 'back',
-                visible: false
-            });
-            break;
-        case 'step2':
-            $('#step2').show();
-            connection.trigger('updateButton', {
-                button: 'back',
-                visible: true
-            });
-            connection.trigger('updateButton', {
-                button: 'next',
-                text: 'next',
-                visible: true
-            });
-            break;
-        case 'step3':
-            $('#step3').show();
-            connection.trigger('updateButton', {
-                button: 'back',
-                visible: true
-            });
-            if (lastStepEnabled) {
-                connection.trigger('updateButton', {
-                    button: 'next',
-                    text: 'next',
-                    visible: true
-                });
-            } else {
-                connection.trigger('updateButton', {
-                    button: 'next',
-                    text: 'done',
-                    visible: true
-                });
-            }
-            break;
-        case 'step4':
-            $('#step4').show();
-            break;
-    }
-}
-
+    connection.trigger('updateActivity', payload);
 });
 
